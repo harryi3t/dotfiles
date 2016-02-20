f8ed1b830932b396aa188e99d53f2d6985ccf15c
harryi3t <66hari@gmail.com>
2016-02-20 15:26:05 +0530

    adds pathogen

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,4 +1,6 @@
 rm -rf ~/.vim/*
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 mkdir -p ~/.vim/colors
